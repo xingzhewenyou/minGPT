5c6f6017f38a1f77011717d2e033793db36f94d3
xingzhewenyou <382524531@qq.com>
2023-04-09 21:39:17 +0800

    读取pt文件，第二次优化

--- projects/adder/out/read_pt.py
+++ projects/adder/out/read_pt.py
@@ -2,12 +2,13 @@ from torchsummary import summary
 from torchvision.models import vgg16  # 以 vgg16 为例
 import torch
 
-myNet = vgg16()  # 实例化网络，可以换成自己的网络
-summary(myNet, (3, 64, 64))  # 输出网络结构
-
-
 path = '/Users/zhangwenyou/PycharmProjects/AIstudy4/minGPT/projects/adder/out/adder/model.pt'
 pretrained_dict = torch.load(path)
+
+myNet = vgg16()  # 实例化网络，可以换成自己的网络
+# myNet = torch.load(path)
+summary(myNet, (3, 64, 64))  # 输出网络结构
+
 for k, v in pretrained_dict.items():  # k 参数名 v 对应参数值
-        print('k==========', k)
-        print('v===================', v)
+    print('k==========', k)
+    print('v===================', v)
