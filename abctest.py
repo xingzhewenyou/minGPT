16124c0d44642a642f5acba92f02836597cf4740
xingzhewenyou <382524531@qq.com>
2023-04-09 14:14:59 +0800

    这个GPT2的最小系统看起来很靠谱，好好学

---
+++ abctest.py
@@ -0,0 +1,6 @@
+from mingpt.model import GPT
+model_config = GPT.get_default_config()
+model_config.model_type = 'gpt2'
+model_config.vocab_size = 50257 # openai's model vocabulary
+model_config.block_size = 1024  # openai's model block_size (i.e. input context length)
+model = GPT(model_config)
